de8d58a0c06a4364088034787a7a5bfe5eb3c4e2
Smile952 <deniska1234denis@gamil.com>
2023-12-11 03:24:07 +0700

    added the test-array with index of items

--- Markov'sChains/Markov'sChains.cpp
+++ Markov'sChains/Markov'sChains.cpp
@@ -5,46 +5,57 @@ using namespace std;
 
 int n = 7;
 
-int main() {
-    float input[7][7] = { {0.9,   0,   0, 0.9,  0,   0, 0.1},
-                         {  0, 0.3,   0,   0,   0,   0, 0.2},
-                         {  0, 0.2, 0.9, 0.1,   0,   0,   0},
-                         {0.1, 0.2, 0.1,   0,   0,   0, 0.1},
-                         {  0,   0,   0,   0, 0.9, 0.8, 0.1},
-                         {  0,   0,   0,   0, 0.1, 0.2,   0},
-                         {  0, 0.3,   0,   0,   0,   0, 0.5} };
+int func(int i, int input[7][7]) {
+    for (int j = 0; j < n; j++) {
+        if(input[i, j] != 0 && i != j) {
+            return j+1;
+        }
+    }
+    return 0;
+}
 
-    float el = 0;
-    int n = 7;
+int main() {
+    int input[7][7] = { {9, 0, 0, 9, 0, 0, 1},
+                        {0, 3, 0, 0, 0, 0, 2},
+                        {0, 2, 9, 1, 0, 0, 0},
+                        {1, 2, 1, 0, 0, 0, 1},
+                        {0, 0, 0, 0, 9, 8, 1},
+                        {0, 0, 0, 0, 1, 2, 0},
+                        {0, 3, 0, 0, 0, 0, 5}
+    };
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            int temp = 0;
+            temp = input[i][j];
+            input[i][j] = input[j][i];
+            input[j][i] = temp;
+        }
+    }
 
     int exc[7]{};
 
-    //Заполнение массива
-    /*for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> el;
-            input[i][j] == el;
-            el = 0;
-        }
-    }*/
+    map<int, int(*)[7]> s;
 
-
-    //Проход по заполненной матрице
+    int test[7] = {};
+    int k = 0;
     for (int i = 0; i < n; i++) {
+        k++;
         for (int j = 0; j < n; j++) {
-            //Если нашел страну для торгов, исключая саму себя
+            //Если нашел страну для торгов, не включая саму себя
             if (i != j && input[i][j] != 0) {
                 //Проверить ее на наличие в исключениях
-                check_exception(exc, j);
+                if (exc[j] == 0) {
+                    exc[i] = 1;
+                    int temp = func(j, input);
+                    if (temp != 0)
+                        test[k] = temp;
+                }
             }
         }
     }
-}
 
-bool check_exception(int *exc[7], int exc_item) {
-    if (exc[exc_item] != 0) {
-        return true;
+    for (int i = 0; i < 7; i++) {
+        cout << test[i] << " ";
     }
-    else exc[exc_item] == 0;
-    return false;
 }
