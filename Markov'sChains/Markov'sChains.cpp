af57bbddeae4d10238858ccf0c1ddb82f38644db
Smile952 <deniska1234denis@gamil.com>
2023-12-20 23:09:24 +0700

    Finally i do it

--- Markov'sChains/Markov'sChains.cpp
+++ Markov'sChains/Markov'sChains.cpp
@@ -2,92 +2,21 @@
 #include<map>
 
 using namespace std;
-
-<<<<<<< HEAD
 int n = 7;
 int k = 0;
 
-void creatASet(int i, int input[7][7], int *clone_arr[49], int exc[7]) {
+void creatASet(int i, int input[7][7], int* clone_arr[49], int exc[7]) {
     for (int j = 0; j < n; j++) {
-        if(input[i][j] != 0 && i != j) {
-            if (exc[j] == 0){
+        if (input[i][j] != 0 && i != j) {
+            if (exc[j] == 0) {
                 exc[j] = 1;
-                *clone_arr[k] = j+1;
+                *clone_arr[k] = j + 1;
                 k++;
                 creatASet(j, input, clone_arr, exc);
-=======
-int check(float input[7][7], int i) {
-
-    for (int j = 0; j < 7; j++) {
-        if (input[i][j] != 0 && i != j) {
-            return j;
-        }
-    }
-
-    return 0;
-}
-
-int main() {
-    float input[7][7] = {
-        {0.9, 0.0, 0.0, 0.9, 0.0, 0.0, 0.1},
-        {0.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.2},
-        {0.0, 0.2, 0.9, 0.1, 0.0, 0.0, 0.0},
-        {0.1, 0.2, 0.1, 0.0, 0.0, 0.0, 0.1},
-        {0.0, 0.0, 0.0, 0.0, 0.9, 0.8, 0.1},
-        {0.0, 0.0, 0.0, 0.0, 0.1, 0.2, 0.0},
-        {0.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.5}
-    };
-
-    // Проверка условия: "Сумма чисел в каждом столбце не должна превышать единицу."
-    //===============================================================================================
-    bool fl = true;
-
-    for (int i = 0; i < 7; i++) {
-
-        float sum = 0;
-
-        for (int j = 0; j < 7; j++) {
-            sum += input[j][i];
-        }
-
-        (sum > 1) ? fl = false : fl;
-    }
-
-    if (fl)
-        cout << "Сумма чисел в каждом столбце не превышает единицу." << endl;
-    else
-        cout << "Сумма чисел в одном или нескольких столбцах превышает единицу." << endl;
-    //==============================================================================================
-
-    
-    float el = 0;
-    int n = 7;
-
-    int temp[7] = {};
-    map<int, int[]> s = {};
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> el;
-            input[i][j] == el;
-            el = 0;
-        }
-    }
-
-    int k = 0; int l = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (input[i][j] != 0 && i != j) {
-                s[l[k++]] = check(input, i); // Здесь хотел исправить, но я не работал с "map",
-                                             // поэтому не знаю, что здесь не так.
->>>>>>> d14ab49d445bdef46bdaad0e41116d1828a31949
             }
         }
     }
 }
-<<<<<<< HEAD
-
 int main() {
     int input[7][7] = { {2, 0, 0, 0, 0, 0, 1},
                         {0, 5, 0, 9, 3, 0, 0},
@@ -161,5 +90,3 @@ int main() {
         cout << endl;
     }
 }
-=======
->>>>>>> d14ab49d445bdef46bdaad0e41116d1828a31949
